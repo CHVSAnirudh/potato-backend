f64dc4372c704c49f6fc462d56b0fcf016e2688f
Arkajit-techie <61142632+Arkajit-techie@users.noreply.github.com>
2022-01-06 23:04:33 +0530

    Adding the Signup function

--- function_calls.py
+++ function_calls.py
@@ -5,6 +5,7 @@ client = MongoClient("mongodb+srv://arkajit:arkajit@cluster0.dmii1.mongodb.net/m
 db = client.get_database('Potato')
 users_collection = db.users
 listings_collection = db.listings
+
 def login(email,password):
     user = users_collection.find_one({'email':email})
     if len(user)==0:
@@ -14,4 +15,20 @@ def login(email,password):
             return "login"
         else:
             return "incorrect email or password"
-print(login('arkajitdatta2010@gmail.com','12345678'))
+# print(login('arkajitdatta2010@gmail.com','12345678'))
+
+#signup function 
+def signup(name,email,password):
+    new_register = {
+        "name" : name,
+        "email" : email,
+        "password" : password
+    }
+    insert = users_collection.insert_one(new_register)
+
+    if insert:
+        return "succcessfull"
+    else:
+        return "error"
+
+# print(signup("Anirudh","ani1005sai@gmail.com","12345678"))
